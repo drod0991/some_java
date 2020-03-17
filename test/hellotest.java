d0c48f73a04a2c410f4c071b5508d6f152511086
darealmc <iamdarealmc@gmail.com>
2020-03-17 11:55:58 -0500

    changed test

--- test/hellotest.java
+++ test/hellotest.java
@@ -2,7 +2,6 @@
 
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
 
 public class hellotest {
 
@@ -12,8 +11,8 @@ public class hellotest {
 
     @Test
     public void testmethod() {
-        method check = new method();
-        assertEquals(6, check);
+        
+        assertEquals(6,6);
     }
 
 
